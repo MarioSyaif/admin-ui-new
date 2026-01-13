a5824e41b99332731c5f77bb4ece587b71a46dbe
MarioSyaif <mariosyaif@gmail.com>
2026-01-14 00:49:47 +0700

    UAS

--- src/components/fragments/CardGoal.jsx
+++ src/components/fragments/CardGoal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../elements/Card";
-import Icon from "../Elements/Icon";
+import Icon from "../elements/Icon";
 import CompositionExample from "../elements/CompositionExample";
 
 function CardGoal(props) {
